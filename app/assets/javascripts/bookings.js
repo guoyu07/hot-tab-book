545a82418ef65225cd0c62adc56cf8bbefcc6ca0
ketan-webonise <ketan@weboniselab.com>
2013-08-04 02:43:52 +0530

    fancybox added to booking

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+  // registration form 
+  $(".topTable").fancybox({
+          
+      });
+  // hover table popup
+  $(".topTable").hover(function(){
+  	$(this).siblings('div').show();
+  },function() {
+    $(".topTable").siblings('div').hide();
+  });
 });
