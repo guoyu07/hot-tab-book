96afacc630fe646ee13e31cc3af952e3caed4917
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-08-03 17:27:31 +0530

    routes added for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 HotTabBook::Application.routes.draw do
   devise_for :users
-
+  scope :module => "admin" do
+  resources :homes do
+    collection  do
+      get :dashboard
+    end
+  end
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,8 +57,9 @@ HotTabBook::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'devise/registrations#new'
-
-
+  devise_scope :user do
+    match'/admin', :to => "devise/sessions#new"
+  end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
